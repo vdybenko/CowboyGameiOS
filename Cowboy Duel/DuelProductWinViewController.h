6f5d8ba5a5a96aa2aa776ed430eb942c5d45ed08
Taras <taras@webkate.com>
2012-10-29 19:58:42 +0200

    rev #121 Дизайн подсказка конец дуели
    Signed-off-by: Taras <taras@webkate.com>

---
+++ Cowboy Duel/DuelProductWinViewController.h
@@ -0,0 +1,13 @@
+//
+//  DuelProductWinViewController.h
+//  Cowboy Duels
+//
+//  Created by Taras on 29.10.12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DuelProductWinViewController : UIViewController
+
+@end
---
+++ Cowboy Duel/DuelProductWinViewController.m
@@ -0,0 +1,38 @@
+//
+//  DuelProductWinViewController.m
+//  Cowboy Duels
+//
+//  Created by Taras on 29.10.12.
+//
+//
+
+#import "DuelProductWinViewController.h"
+
+@interface DuelProductWinViewController ()
+
+@end
+
+@implementation DuelProductWinViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
