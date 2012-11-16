997787fdc9c0be00674885597fcb13e9475531d6
sobol@webkate.com <sobol@webkate.com>
2012-11-16 11:13:49 +0200

    #215 Login screan design for iPhone 5

--- Cowboy Duel/LoginAnimatedViewController.m
+++ Cowboy Duel/LoginAnimatedViewController.m
@@ -23,6 +23,9 @@ NSString *const URL_PAGE_IPAD_COMPETITION=@"http://cdfb.webkate.com/contest/firs
     NSMutableString *stDonate;
     __unsafe_unretained IBOutlet UIView *activityView;
     __unsafe_unretained IBOutlet UIActivityIndicatorView *activityIndicatorView;
+    __unsafe_unretained IBOutlet UIImageView *backgroundView;
+    __unsafe_unretained IBOutlet UIImageView *boardImage;
+    __unsafe_unretained IBOutlet UIImageView *tryAgainImage;
     BOOL tryAgain;
     CGRect guillBackUp;
 //    int counterTryAgain;
@@ -101,6 +104,25 @@ static LoginAnimatedViewController *sharedHelper = nil;
 -(void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+    if (self.view.frame.size.height > 480) {
+        [backgroundView setImage:[UIImage imageNamed:@"la_bg-568h.png"]];
+        [boardImage setImage:[UIImage imageNamed:@"la_board-568h.png"]];
+        CGRect frame = CGRectMake(0, 356, 320, 212);
+        [boardImage setFrame:frame];
+        [tryAgainImage setImage:[UIImage imageNamed:@"la_ta_bg-568h.png"]];
+        frame = self.guillotineImage.frame;
+        frame.size.height = 558;
+        frame.origin.y = -400;
+        self.guillotineImage.frame = frame;
+        [self.guillotineImage setImage:[UIImage imageNamed:@"ivGuillotineFull-568h.png"]];
+        
+    }
+    else {
+        [backgroundView setImage:[UIImage imageNamed:@"la_bg.png"]];
+        [boardImage setImage:[UIImage imageNamed:@"la_board.png"]];
+        [tryAgainImage setImage:[UIImage imageNamed:@"la_ta_bg.png"]];
+        [self.guillotineImage setImage:[UIImage imageNamed:@"ivGuillotineFull.png"]];
+    }
     self.loginLable.text = NSLocalizedString(@"LOGIN", @"");
     self.loginLable.font = [UIFont fontWithName: @"DecreeNarrow" size:24];
     
@@ -550,6 +572,9 @@ static LoginAnimatedViewController *sharedHelper = nil;
     [CATransaction begin];
     [self.view.layer removeAllAnimations];
     [CATransaction commit];
+    backgroundView = nil;
+    boardImage = nil;
+    tryAgainImage = nil;
     [super viewDidUnload];
 }
 @end
