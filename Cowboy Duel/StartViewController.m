1e0d8910a6dd31030dafae3b91d79d70574943f1
sobol@webkate.com <sobol@webkate.com>
2013-01-25 11:42:50 +0200

    fix hudView

--- Cowboy Duel/StartViewController.m
+++ Cowboy Duel/StartViewController.m
@@ -491,9 +491,7 @@ static StartViewController *sharedHelper = nil;
     if (![[NSUserDefaults standardUserDefaults] boolForKey:@"FirstRun_v2.2"]) {
         [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"FirstRun_v2.2"];
         [[NSUserDefaults standardUserDefaults] synchronize];
-        UIImageView_AttachedView *arrowImage3=[[UIImageView_AttachedView alloc] initWithImage:[UIImage imageNamed:@"st_arrow.png"] attachedToFrame:duelButton frequence:0.5 amplitude:10 direction:DirectionToAnimateLeft];
-
-        [arrowImage3 startAnimation];
+        
         [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"FirstRunForStore_v2.2"];
     }
     else
@@ -501,10 +499,7 @@ static StartViewController *sharedHelper = nil;
         if ([[NSUserDefaults standardUserDefaults] boolForKey:@"FirstRunForStore_v2.2"])
         {
             [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"FirstRunForStore_v2.2"];
-            int saloonIndex = [self.view.subviews indexOfObject:duelButton];
-            int storeIndex = [self.view.subviews indexOfObject:mapButton];
-            [self.view exchangeSubviewAtIndex:storeIndex withSubviewAtIndex:saloonIndex];
-            UIImageView_AttachedView *arrowImage3=[[UIImageView_AttachedView alloc] initWithImage:[UIImage imageNamed:@"st_arrow.png"] attachedToFrame:mapButton frequence:0.5 amplitude:10 direction:DirectionToAnimateLeft];
+            
         }
     }
         
