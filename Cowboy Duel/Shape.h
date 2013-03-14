ecd895b7b7d2229b7a25ffc3319e94678219e6c2
Taras <taras@webkate.com>
2013-03-14 18:04:27 +0200

    #613 opponent make it OOP

---
+++ Cowboy Duel/Shape.h
@@ -0,0 +1,17 @@
+//
+//  WomanShape.h
+//  Bounty Hunter
+//
+//  Created by Taras on 12.03.13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Shape : UIView<MemoryManagement>
+@property (weak, nonatomic) IBOutlet UIImageView *imageMain;
+
+-(id)initWithCoder:(NSCoder *)aDecoder subViewFromNibFileName:(NSString*)name;
+-(void)randomPositionWithView:(UIView*)view;
+-(BOOL)shotInShapeWithPoint:(CGPoint)point superViewOfPoint:(UIView *)view;
+@end
