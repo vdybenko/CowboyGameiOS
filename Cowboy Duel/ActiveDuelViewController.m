93ed2156f55fe4ceb33317116f345ed57dc57e0a
Taras <taras@webkate.com>
2013-03-15 17:07:11 +0200

    first login change duration of text

--- Cowboy Duel/ActiveDuelViewController.m
+++ Cowboy Duel/ActiveDuelViewController.m
@@ -869,8 +869,8 @@ static CGFloat blinkBottomOriginY;
 -(void)moveOponent
 {
     [opponentShape performSelectorInBackground:@selector(moveOponentInBackground) withObject:nil];
-    [womanShape moveWoman];
-    [goodCowboyShape moveGoodCowboy];
+   [womanShape moveWoman];
+   [goodCowboyShape moveGoodCowboy];
 }
 
 -(void)setScale
