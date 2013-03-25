384736e098bf13163f4c0679820a085b7500491e
sobol@webkate.com <sobol@webkate.com>
2013-03-25 17:06:08 +0200

    #653

---
+++ Cowboy Duel/CDBarrierDuelProduct.h
@@ -0,0 +1,13 @@
+//
+//  CDBarrierDuelProduct.h
+//  Bounty Hunter
+//
+//  Created by Sergey Sobol on 25.03.13.
+//
+//
+
+#import "CDDuelProduct.h"
+
+@interface CDBarrierDuelProduct : CDDuelProduct
+@property(nonatomic) NSInteger dType;
+@end
---
+++ Cowboy Duel/CDBarrierDuelProduct.m
@@ -0,0 +1,33 @@
+//
+//  CDBarrierDuelProduct.m
+//  Bounty Hunter
+//
+//  Created by Sergey Sobol on 25.03.13.
+//
+//
+
+#import "CDBarrierDuelProduct.h"
+
+@implementation CDBarrierDuelProduct
+@synthesize dType;
+-(void)encodeWithCoder:(NSCoder *)encoder
+{
+    [super encodeWithCoder:encoder];
+    [encoder encodeInteger:self.dType forKey:@"TYPE"];
+}
+
+-(id)initWithCoder:(NSCoder *)decoder
+{
+    self = [super initWithCoder:decoder];
+    self.dType = [decoder decodeIntegerForKey:@"TYPE"];
+    return self;
+}
+
+-(id)init
+{
+    self = [super init];
+    self.dType = -1;
+    return self;
+}
+
+@end
