e5cf1330e8405a3b5442883f7867af34d5cbbf12
Pasha <pashaxcod@gmail.com>
2013-05-10 16:55:36 +0300

    create blocks

--- Cowboy Duel/BuilderViewController.h
+++ Cowboy Duel/BuilderViewController.h
@@ -10,7 +10,10 @@
 @class VisualViewDataSource;
 
 @interface BuilderViewController : UIViewController<MemoryManagement,UICollectionViewDataSource,UICollectionViewDelegate>
+typedef void (^ScrollViewSwitcherResult)(NSInteger curentIndex);
+typedef void (^BuyActionBlock)(NSInteger curentIndex);
 
 @property (strong, nonatomic) VisualViewDataSource *visualViewDataSource;
-
+@property (copy) ScrollViewSwitcherResult didFinishBlock;
+@property (copy) BuyActionBlock didBuyAction;
 @end
