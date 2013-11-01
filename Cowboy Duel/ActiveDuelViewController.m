15a9f56d16cc340f833b340e6131b21e3aea1e3f
Taras <taras@webkate.com>
2013-11-01 14:51:03 +0200

    #819 - Final VC: make horizontal

--- Cowboy Duel/ActiveDuelViewController.m
+++ Cowboy Duel/ActiveDuelViewController.m
@@ -417,9 +417,8 @@ static CGFloat blinkBottomOriginY;
     {
         blurredBack = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bloorOnBlackView.png"]];
         blurredBack.hidden = YES;
-        int iPhone5Delta = [UIScreen mainScreen].bounds.size.height - 480;
         CGRect deltaFrame = blurredBack.frame;
-        deltaFrame.size.height += iPhone5Delta;
+        deltaFrame.size.width = sizeMainScreen.height;
         blurredBack.frame = deltaFrame;
         [self.view addSubview:blurredBack];
         
